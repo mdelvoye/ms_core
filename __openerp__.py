83535c86c8f6f1df1ba2f6bf488f69aaffc83b0b
Michael Delvoye <mdelvoye@misyl-services.com>
2016-08-08 17:39:51 +0200

    Version 9.0.1.0

---
+++ __openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name" : "MS Core",
+    "version" : "9.0.1.0",
+    "category" : "Base",
+    'author': u'Misyl Services',
+    'depends' : ['base'],
+    "data" : [
+              'views/res_partner_view.xml',
+              'views/ms_core.xml',
+             ],
+    'installable': True,
+    'auto_install': False,
+}
